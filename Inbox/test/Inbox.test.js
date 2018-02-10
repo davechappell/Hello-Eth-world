d02a248f879af784808512b6f91809d051186020
Dave Chappell <Dave.Chappell.1@gmail.com>
2018-02-10 14:02:35 -0500

    Updates

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -1,4 +1,56 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // use one of those account to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Say hello to your mother!']})
+    .send({ from: accounts[0], gas: '1000000'});
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+});
+
+/*class Car {
+  park(){
+    return 'stopped';
+  }
+
+  drive(){
+    return 'vroom';
+  }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car Class dkdk', () => {
+  it('can park', ()=> {
+    assert.equal(car.park(), 'stopped');
+  });
+
+  it('can drive', () => {
+    assert.equal(car.drive(),'vroom');
+  });
+
+
+});
+*/
